547ddb2c1d966c8626cb9f9755a7a9b880707edc
dlavilat <48604347+dlavilat@users.noreply.github.com>
2021-12-13 16:48:29 -0500

    se modifica archivo pom.xml

--- src/test/java/com/choucair/formacion/RunnerTags.java
+++ src/test/java/com/choucair/formacion/RunnerTags.java
@@ -8,9 +8,9 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 @RunWith(CucumberWithSerenity.class)
 //@CucumberOptions (features = "src/test/resources/features/", tags = "@Regresion")
 //@CucumberOptions (features = "src/test/resources/features/Login/login.feature", tags = "@Login")
-//@CucumberOptions (features = "src/test/resources/features/TransferMoney/transferMoney.feature", tags = "@TransferMoney")
+@CucumberOptions (features = "src/test/resources/features/TransferMoney/transferMoney.feature", tags = "@TransferMoney")
 //@CucumberOptions (features = "src/test/resources/features/PayBills/paySavedPayee.feature", tags = "@PaySavedPayee")
-@CucumberOptions (features = "src/test/resources/features/PayBills/payee.feature", tags = "@Payee")
+//@CucumberOptions (features = "src/test/resources/features/PayBills/payee.feature", tags = "@Payee")
 public class RunnerTags {
 
 }
